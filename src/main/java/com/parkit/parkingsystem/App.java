00d7bd2bdb5e8b6ced1b4536f5850ff6f18fc034
Muriel <proton.muriel@outlook.fr>
2021-11-22 15:49:58 +0100

    updating Main App

--- src/main/java/com/parkit/parkingsystem/App.java
+++ src/main/java/com/parkit/parkingsystem/App.java
@@ -1,13 +1,21 @@
 package com.parkit.parkingsystem;
 
-import com.parkit.parkingsystem.service.InteractiveShell;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.service.FareCalculatorService;
+
+import org.apache.commons.lang.ObjectUtils.Null;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class App {
-    private static final Logger logger = LogManager.getLogger("App");
-    public static void main(String args[]){
-        logger.info("Initializing Parking System");
-        InteractiveShell.loadInterface();
+    //private static final Logger logger = LogManager.getLogger("App");
+    public void main(String args[]){
+       logger.info("Initializing Parking System");
+       InteractiveShell.loadInterface();
+
     }
 }
