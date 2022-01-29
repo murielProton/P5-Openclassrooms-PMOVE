9d04e65b3a9b1e84773d35d2b8631f8511b2294a
OHMU <proton.muriel@outlook.fr>
2022-01-29 13:13:18 +0100

    AlphaControllerTest completed Successfully run 29/01/2022

--- src/test/java/com/parkit/parkingsystem/AlphaControllerTest.java
+++ src/test/java/com/parkit/parkingsystem/AlphaControllerTest.java
@@ -1,48 +1,111 @@
 package com.parkit.parkingsystem;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.controller.AlphaController;
+import com.parkit.parkingsystem.dao.ParkingSpotDAO;
+import com.parkit.parkingsystem.dao.TicketDAO;
+import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 
+import junit.framework.Assert;
+
 public class AlphaControllerTest {
+	
+	private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
+	private static DataBasePrepareService dataBasePrepareService = new DataBasePrepareService();
+
+	@BeforeEach
+    private void setUpPerTest() throws Exception {
+    	ParkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
+		TicketDAO.dataBaseConfig = dataBaseTestConfig;
+    	dataBasePrepareService.clearDataBaseEntries();
+    }
 	@Test
     public void  myTestAlphaControllerExistWithoutInteraction() {
-    	
-    	//GIVEN
-    	// Setup
-    	//WHEN
-    	// appel à ce qu'il faut tester
-    	//THEN
-    	// Controle.
     	try (MockedStatic<InputReaderUtil> mockedInput = Mockito.mockStatic(InputReaderUtil.class)) {
-//    		//Pour tester l'AlphaController
-    		//1- faire la liste de tous les input que l'application va demander
-    		//2- appeler AlphaController.loadInterface();
-    		//3- faire ls assert equals qui vont bien
-    		//comme fait dans l'exemple ci après
-    		//NB dans le cas où il y a plus de InputReaderUtil.readInt() ou readNextLine()
-    		// que de thenReturn 
-    		// le InputReaderUtil retournera la valeur du dernier thenReturn. 
-    		// dans le cas présent "call3"
-    		//mockedInput.when(InputReaderUtil::readNextLine)
-//    		.thenReturn("call1")
-//    		.thenReturn("call2")
-//    		.thenReturn("call3");
+    		// GIVEN
     		mockedInput.when(InputReaderUtil::readInt)
-    		
     		.thenReturn(3);
     		// Les appels qui vons être fait par le test
+    		
+    		//WHEN
     		AlphaController.loadInterface();
-//    		System.out.println(InputReaderUtil.readInt());
-//    		System.out.println(InputReaderUtil.readNextLine());
-//    		System.out.println(InputReaderUtil.readNextLine());
-//    		System.out.println(InputReaderUtil.readNextLine());
-//    		System.out.println(InputReaderUtil.readInt());
-//    		System.out.println(InputReaderUtil.readNextLine());
-//    		System.out.println(InputReaderUtil.readNextLine());
+    		// THEN
+    		Assert.assertEquals(4,new ParkingSpotDAO().getNextAvailableSlot(ParkingType.BIKE));
+    		Assert.assertEquals(1,new ParkingSpotDAO().getNextAvailableSlot(ParkingType.CAR));
+    	}
+    }
+	
+	@Test
+    public void  myTestAlphaControllerEntering() {
+		String regNumberCar = "123456789";
+    	try (MockedStatic<InputReaderUtil> mockedInput = Mockito.mockStatic(InputReaderUtil.class)) {
+    		// GIVEN
+    		// Les appels qui vons être fait par le test
+    		mockedInput.when(InputReaderUtil::readInt)
+    		.thenReturn(1)// Enter
+    		.thenReturn(1) // CAR
+    		.thenReturn(3);// Exit app
+			mockedInput.when(InputReaderUtil::readNextLine)
+				.thenReturn(regNumberCar); // car for enter
+
+    		
+    		//WHEN
+    		AlphaController.loadInterface();
+    		// THEN
+    		Assert.assertEquals(4,new ParkingSpotDAO().getNextAvailableSlot(ParkingType.BIKE));
+    		Assert.assertEquals(2,new ParkingSpotDAO().getNextAvailableSlot(ParkingType.CAR));
+    	}
+    }
+	
+	@Test
+    public void  myTestAlphaControllerEnteringAndExitingCar() {
+		String regNumberCar = "123456789";
+    	try (MockedStatic<InputReaderUtil> mockedInput = Mockito.mockStatic(InputReaderUtil.class)) {
+    		// GIVEN
+    		// Les appels qui vons être fait par le test
+    		mockedInput.when(InputReaderUtil::readInt)
+    		.thenReturn(1)// Enter
+    		.thenReturn(1) // CAR
+    		.thenReturn(2) // Exit car
+    		.thenReturn(3); // Exit app
+			mockedInput.when(InputReaderUtil::readNextLine)
+			.thenReturn(regNumberCar) // car for enter
+			.thenReturn(regNumberCar);// car for exit
+    		
+    		//WHEN
+    		AlphaController.loadInterface();
+    		// THEN
+    		Assert.assertEquals(4,new ParkingSpotDAO().getNextAvailableSlot(ParkingType.BIKE));
+    		Assert.assertEquals(1,new ParkingSpotDAO().getNextAvailableSlot(ParkingType.CAR));
+    	}
+    }
+	
+	@Test
+    public void  myTestAlphaControllerEnteringAndExitingBike() {
+		String regNumberBike = "123456789";
+    	try (MockedStatic<InputReaderUtil> mockedInput = Mockito.mockStatic(InputReaderUtil.class)) {
+    		// GIVEN
+    		// Les appels qui vons être fait par le test
+    		mockedInput.when(InputReaderUtil::readInt)
+    		.thenReturn(1) // Enter
+    		.thenReturn(2) // BIKE
+    		.thenReturn(2) // exit parking
+    		.thenReturn(3); // Exit app
+			mockedInput.when(InputReaderUtil::readNextLine)
+			.thenReturn(regNumberBike) // bike for enter
+			.thenReturn(regNumberBike); // bike for exit
+    		
+    		//WHEN
+    		AlphaController.loadInterface();
+    		// THEN
+    		Assert.assertEquals(4,new ParkingSpotDAO().getNextAvailableSlot(ParkingType.BIKE));
+    		Assert.assertEquals(1,new ParkingSpotDAO().getNextAvailableSlot(ParkingType.CAR));
     	}
     }
 }
