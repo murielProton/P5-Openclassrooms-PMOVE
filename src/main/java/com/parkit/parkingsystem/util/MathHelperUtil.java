067aecedaac4bc3592b1bd59424f66a2217cacc4
OHMU <proton.muriel@outlook.fr>
2022-01-31 15:01:10 +0100

    A method test is rarely static correction of method from static to non static

--- src/main/java/com/parkit/parkingsystem/util/MathHelperUtil.java
+++ src/main/java/com/parkit/parkingsystem/util/MathHelperUtil.java
@@ -1,7 +1,7 @@
 package com.parkit.parkingsystem.util;
 
 public class MathHelperUtil {
-	  public static double roundingPrice(Double doubleToRound)
+	  public static double roundingPrice(double doubleToRound)
 	  {
 	      double roundedDouble = Math.round(doubleToRound*100.0)/100.0;
 	      return roundedDouble;
--- src/test/java/com/parkit/parkingsystem/MathHelperUtilTest.java
+++ src/test/java/com/parkit/parkingsystem/MathHelperUtilTest.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathHelperUtilTest {
+    /**
+     * @author Muriel Proton
+     * Test in class MathHelperUtil the method roundingPrice(DOUBLE)
+     * run with success the 31 01 2022 at h
+     */
 	@Test
-	public static void roundingPriceTest()
-	  {
+	public void roundingPriceTest(){
 		double doubleToRound = 3.4499999999999997;
 		double expectedDouble = 3.45;
 		double doubleToTest = MathHelperUtil.roundingPrice(doubleToRound);
